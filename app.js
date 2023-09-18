9fc686282843eab2644a8c5d415f9d71f6a4bea2
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-09-19 02:03:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+// Q:1
+// for (let i = 0; i < 10; i++) {
+// alert(i=i)
+// }
+
+// Q:2
+// for (let i = 0; i <= 11; i++) {
+//     alert(i=i)
+// }
+
+// Q:3
+// for (var i = 0; i <= 4; i++){
+//     alert(i+i)
+// }
+
+// Q:4
+
+// for (var count = 0; count < 100; count++) {
+//    alert(count=count)
+// }
+
+// Q:5
+
+// for (var i = 3; i > 0; i--) {
+//     alert(i)
+
+// }
+
+// Q:6
+
+// count = len(myArray)
+
+// Q:7
+
+// flag = True
+
+// Q:8
+
+// pets = ["dog", "cat", "rabbit", "hamster"]
+
+// for i in (len(pets)):
+
+    // print(pets[i])
+// Q:10    
+    // usernames = ["Alice", "Bob", "Charlie", "David"]
+    // firstname = input("Enter first name")
+    // for i in range(len(user_names))
+    //     if firstname == user_names[i]
+    //         print("Welcome,", firstname)
+    // else:
+        // print("Please write a correct user name.")
+    
+        // Q:11
+
+//         var matchFound = false;
+// var list = ["pop", "Bob", "Charlie", "David"];
+
+// var userInput = prompt("Enter first name:");
+
+// for (var i = 0; i < list.length; i++) {
+//   if (userInput === list[i]) {
+//     alert("Match found");
+//     matchFound = true;
+//     break;
+//   }
+// }
+
+// if (!matchFound) {
+//   alert("Please write a correct user name.");
+// }
+
+
+
+
